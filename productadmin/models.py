d4f8f94f46ba57b11775ac5e356febb24b8e2e08
AJAY E <ajaye2901@gmail.com>
2024-07-10 15:49:15 +0530

    added offer feature

--- productadmin/models.py
+++ productadmin/models.py
@@ -44,6 +44,7 @@ class ProductVariant(models.Model):
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     color = models.ForeignKey(Color, on_delete=models.CASCADE)
     actual_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    discount_percentage = models.PositiveIntegerField(default=0)
     discount_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     stock = models.PositiveIntegerField(default=0)
     is_featured = models.BooleanField(default=False)
@@ -53,5 +54,11 @@ class ProductVariant(models.Model):
 
     def __str__(self):
         return f"{self.product.name} - {self.color} - {self.size}"
+    
+    def save(self, *args, **kwargs) :
+        if self.actual_price and self.discount_percentage :
+            discount_amount = (self.discount_percentage / 100) * self.actual_price
+            self.discount_price = self.actual_price - discount_amount
+        super().save(*args, **kwargs)
     
     
